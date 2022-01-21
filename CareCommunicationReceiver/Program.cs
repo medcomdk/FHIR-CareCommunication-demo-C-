5c13119813248e90f00a6fadeaa7ffa79faccbc2
Torben M. Hagensen <tmh@mjolner.dk>
2022-01-21 10:49:49 +0100

    Added Receiver

--- CareCommunicationReceiver/Program.cs
+++ CareCommunicationReceiver/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CareCommunicationProfile;
+using System;
+using System.IO;
+using System.Linq;
+
+var msgFile = args.FirstOrDefault() ?? string.Empty;
+
+if (msgFile == string.Empty)
+{
+    Console.WriteLine("Usage: CareCommunicationReceiver <file name>");
+    return -1;
+}
+
+try
+{
+    var messageText = File.ReadAllText(msgFile);
+
+    var msg = CareCommunicationParser.FromJson(messageText);
+
+    Console.WriteLine($"Sender Name: {msg.Sender.Name}");
+    Console.WriteLine($"  - SOR Code: {msg.Sender.SOR}");
+    Console.WriteLine($"  - EAN Code: {msg.Sender.EAN}");
+    Console.WriteLine();
+    Console.WriteLine($"Receiver Name: {msg.PrimaryReceiver.Name}");
+    Console.WriteLine($"  - SOR Code: {msg.PrimaryReceiver.SOR}");
+    Console.WriteLine($"  - EAN Code: {msg.PrimaryReceiver.EAN}");
+    Console.WriteLine();
+    Console.WriteLine($"Patient Name: {msg.CareCommunication.Subject.Name}");
+    Console.WriteLine($"  - Address (street): {msg.CareCommunication.Subject.Address.FirstOrDefault()?.Line ?? String.Empty}");
+    Console.WriteLine($"  - Address (postal code): {msg.CareCommunication.Subject.Address.FirstOrDefault()?.PostalCode ?? String.Empty}");
+    Console.WriteLine($"  - Address (city): {msg.CareCommunication.Subject.Address.FirstOrDefault()?.City ?? String.Empty}");
+    Console.WriteLine($"  - Deseased: {(msg.CareCommunication.Subject.Deceased ? "Yes" : "No")}");
+    Console.WriteLine();
+    Console.WriteLine($"Category: {msg.CareCommunication.Category}");
+    Console.WriteLine($"Topic: {msg.CareCommunication.Topic ?? String.Empty}");
+    Console.WriteLine();
+    Console.WriteLine($"Timestamp: {msg.CareCommunication.PayloadTexts.FirstOrDefault()?.Timestamp.ToString() ?? string.Empty}");
+    Console.WriteLine($"Author: {msg.CareCommunication.PayloadTexts.FirstOrDefault()?.Author ?? string.Empty}");
+    Console.WriteLine($"Text: {msg.CareCommunication.PayloadTexts.FirstOrDefault()?.Text ?? string.Empty}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+
+return 0;
--- CareCommunicationSender/Program.cs
+++ CareCommunicationSender/Program.cs
@@ -5,35 +5,35 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-var outputDir = args.FirstOrDefault(s => s.StartsWith("/O:"))?.Substring(3) ?? ".";
+var outputDir = args.FirstOrDefault() ?? ".";
 
 Console.Write("Sender Name:");
-var senderName = Console.ReadLine() ?? String.Empty;
-Console.Write("  - SOR code:");
-var senderSORCode = Console.ReadLine() ?? String.Empty;
-Console.Write("  - EAN code:");
-var senderEANCode = Console.ReadLine() ?? String.Empty;
+var senderName = GetInputString("Sender Name");
+Console.Write("  - SOR Code:");
+var senderSORCode = GetInputString("123456789012345");
+Console.Write("  - EAN Code:");
+var senderEANCode = GetInputString("123456789012");
 var sender = new MessagingOrganizationDTO(senderName, senderSORCode, senderEANCode);
 
 Console.Write("Receiver Name:");
-var receiverName = Console.ReadLine() ?? String.Empty;
-Console.Write("  - SOR code:");
-var receiverSORCode = Console.ReadLine() ?? String.Empty;
-Console.Write("  - EAN code:");
-var receiverEANCode = Console.ReadLine() ?? String.Empty;
+var receiverName = GetInputString("Receiver Name");
+Console.Write("  - SOR Code:");
+var receiverSORCode = GetInputString("0987654321098765");
+Console.Write("  - EAN Code:");
+var receiverEANCode = GetInputString("098765432109");
 var receiver = new MessagingOrganizationDTO(receiverName, receiverSORCode, receiverEANCode);
 
 Console.Write("Patient Name:");
-var patientName = Console.ReadLine() ?? String.Empty;
+var patientName = GetInputString("Patient Name");
 Console.Write("  - Address (street):");
-var addressStreet = Console.ReadLine() ?? String.Empty;
+var addressStreet = GetInputString("Street Name X");
 Console.Write("  - Address (postal code):");
-var addressPostalCode = Console.ReadLine() ?? String.Empty;
+var addressPostalCode = GetInputString("Postal Code");
 Console.Write("  - Address (city):");
-var addressCity = Console.ReadLine() ?? String.Empty;
+var addressCity = GetInputString("City");
 var address = new AddressDTO(addressStreet, addressPostalCode, addressCity);
 Console.Write("  - Deseased (Y/N):");
-var deseased = Console.ReadLine()?.First() == 'Y';
+var deseased = GetInputString("Y").ToUpper().StartsWith('Y');
 var patient = new PatientDTO(patientName, new List<AddressDTO> { address }, deseased);
 
 var category = "carecoordination";
@@ -42,12 +42,32 @@ var topic = (string?)null;
 var timestamp = DateTimeOffset.Now;
 var author = "Michael Burns";
 Console.Write("Text:");
-var text = Console.ReadLine() ?? String.Empty;
+var text = GetInputString("Text content");
 var payloadTexts = new List<PayloadTextDTO>() { new PayloadTextDTO(timestamp, author, text) };
 
 var careCommunication = new CareCommunicationDTO(patient, category, topic, payloadTexts);
 var message = new CareCommunicationMessageDTO(sender, receiver, careCommunication);
 
-var json = new CareCommunicationSerializer().SerializeAsJson(message);
+try
+{
+    var fileName = $"{outputDir}\\msg{timestamp.LocalDateTime.ToString("yyyyMMddHHmmssfff")}.json";
+    var json = new CareCommunicationSerializer().SerializeAsJson(message);
 
-File.WriteAllText($"{outputDir}\\msg{timestamp.LocalDateTime.ToString("yyyyMMddHHmmssfff")}.json", json);
+    File.WriteAllText(fileName, json);
+
+    Console.WriteLine($"Generated file: {fileName}");
+
+    return 0;
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+
+    return -1;
+}
+
+static string GetInputString(string defaultValue)
+{
+    var result = Console.ReadLine();
+    return string.IsNullOrWhiteSpace(result) ? defaultValue : result;
+}
